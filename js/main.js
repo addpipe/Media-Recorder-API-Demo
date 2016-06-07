be172c87a62176ce5afef1b6699f2e9b7a79e405
Octavian <octavian@nusofthq.com>
2016-06-07 18:50:15 +0300

    Replaced tabs with spaces as I've seen in Silicon Valley s03e06 Updated MediaRecorder.isTypeSupported desc to reflect that it is now in in the MediaRecorder API spec

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ var downloadLink = document.querySelector('a#downloadLink');
 videoElement.controls = false;
 
 function errorCallback(error){
-  console.log('navigator.getUserMedia error: ', error);
+	console.log('navigator.getUserMedia error: ', error);
 }
 
 /*
@@ -38,88 +38,79 @@ var chunks = [];
 var count = 0;
 
 function startRecording(stream) {
-  log('Start recording...');
-	if (typeof MediaRecorder.isTypeSupported == 'function')
-	{
+	log('Start recording...');
+	if (typeof MediaRecorder.isTypeSupported == 'function'){
 		/*
-			MediaRecorder.isTypeSupported is a Chrome 49 function announced in https://developers.google.com/web/updates/2016/01/mediarecorder but it's not present in the MediaRecorder API spec http://www.w3.org/TR/mediastream-recording/
+			MediaRecorder.isTypeSupported is a function announced in https://developers.google.com/web/updates/2016/01/mediarecorder and later introduced in the MediaRecorder API spec http://www.w3.org/TR/mediastream-recording/
 		*/
 		if (MediaRecorder.isTypeSupported('video/webm;codecs=vp9')) {
-  	  var options = {mimeType: 'video/webm;codecs=vp9'};
-  	} else if (MediaRecorder.isTypeSupported('video/webm;codecs=vp8')) {
-  	  var options = {mimeType: 'video/webm;codecs=vp8'};
-  	}
-  	log('Using '+options.mimeType);
+		  var options = {mimeType: 'video/webm;codecs=vp9'};
+		} else if (MediaRecorder.isTypeSupported('video/webm;codecs=vp8')) {
+		  var options = {mimeType: 'video/webm;codecs=vp8'};
+		}
+		log('Using '+options.mimeType);
 		mediaRecorder = new MediaRecorder(stream, options);
 	}else{
 		log('Using default codecs for browser');
 		mediaRecorder = new MediaRecorder(stream);
 	}
 
+	pauseResBtn.textContent = "Pause";
+
+	mediaRecorder.start(10);
+
+	var url = window.URL || window.webkitURL;
+	videoElement.src = url ? url.createObjectURL(stream) : stream;
+	videoElement.play();
+
+	mediaRecorder.ondataavailable = function(e) {
+		//log('Data available...');
+		//console.log(e.data);
+		//console.log(e.data.type);
+		//console.log(e);
+		chunks.push(e.data);
+	};
+
+	mediaRecorder.onerror = function(e){
+		log('Error: ' + e);
+		console.log('Error: ', e);
+	};
 
 
+	mediaRecorder.onstart = function(){
+		log('Started & state = ' + mediaRecorder.state);
+	};
 
+	mediaRecorder.onstop = function(){
+		log('Stopped  & state = ' + mediaRecorder.state);
 
-  pauseResBtn.textContent = "Pause";
+		var blob = new Blob(chunks, {type: "video/webm"});
+		chunks = [];
 
-  mediaRecorder.start(10);
+		var videoURL = window.URL.createObjectURL(blob);
 
-  var url = window.URL || window.webkitURL;
-  videoElement.src = url ? url.createObjectURL(stream) : stream;
-  videoElement.play();
+		downloadLink.href = videoURL;
+		videoElement.src = videoURL;
+		downloadLink.innerHTML = 'Download video file';
 
-  mediaRecorder.ondataavailable = function(e) {
-    //log('Data available...');
-    //console.log(e.data);
-    //console.log(e.data.type);
-    //console.log(e);
+		var rand =  Math.floor((Math.random() * 10000000));
+		var name  = "video_"+rand+".webm" ;
 
-	chunks.push(e.data);
+		downloadLink.setAttribute( "download", name);
+		downloadLink.setAttribute( "name", name);
+	};
 
+	mediaRecorder.onpause = function(){
+		log('Paused & state = ' + mediaRecorder.state);
+	}
 
-  };
+	mediaRecorder.onresume = function(){
+		log('Resumed  & state = ' + mediaRecorder.state);
+	}
 
-  mediaRecorder.onerror = function(e){
-    log('Error: ' + e);
-    console.log('Error: ', e);
-  };
-
-
-  mediaRecorder.onstart = function(){
-    log('Started & state = ' + mediaRecorder.state);
-  };
-
-  mediaRecorder.onstop = function(){
-    log('Stopped  & state = ' + mediaRecorder.state);
-
-    var blob = new Blob(chunks, {type: "video/webm"});
-    chunks = [];
-
-    var videoURL = window.URL.createObjectURL(blob);
-
-    downloadLink.href = videoURL;
-    videoElement.src = videoURL;
-    downloadLink.innerHTML = 'Download video file';
-
-    var rand =  Math.floor((Math.random() * 10000000));
-    var name  = "video_"+rand+".webm" ;
-
-    downloadLink.setAttribute( "download", name);
-    downloadLink.setAttribute( "name", name);
-
-  };
-
-  mediaRecorder.onpause = function(){
-	  log('Paused & state = ' + mediaRecorder.state);
-  }
-
-  mediaRecorder.onresume = function(){
-	  log('Resumed  & state = ' + mediaRecorder.state);
-  }
-
-  mediaRecorder.onwarning = function(e){
-    log('Warning: ' + e);
-  };
+	mediaRecorder.onwarning = function(e){
+		log('Warning: ' + e);
+	};
 }
 
 //function handleSourceOpen(event) {
@@ -130,16 +121,16 @@ function startRecording(stream) {
 
 function onBtnRecordClicked (){
 	 if (typeof MediaRecorder === 'undefined' || !navigator.getUserMedia) {
-	    alert('MediaRecorder not supported on your browser, use Firefox 30 or Chrome 49 instead.');
-	  }else {
-	    navigator.getUserMedia(constraints, startRecording, errorCallback);
-	    recBtn.disabled = true;
-	    pauseResBtn.disabled = false;
-	    stopBtn.disabled = false;
-	  }
-  }
+		alert('MediaRecorder not supported on your browser, use Firefox 30 or Chrome 49 instead.');
+	}else {
+		navigator.getUserMedia(constraints, startRecording, errorCallback);
+		recBtn.disabled = true;
+		pauseResBtn.disabled = false;
+		stopBtn.disabled = false;
+	}
+}
 
-  function onBtnStopClicked(){
+function onBtnStopClicked(){
 	mediaRecorder.stop();
 	videoElement.controls = true;
 
@@ -149,40 +140,30 @@ function onBtnRecordClicked (){
 }
 
 function onPauseResumeClicked(){
-
 	if(pauseResBtn.textContent === "Pause"){
-
 		console.log("pause");
-
 		pauseResBtn.textContent = "Resume";
 		mediaRecorder.pause();
-
 		stopBtn.disabled = true;
-
 	}else{
 		console.log("resume");
-
 		pauseResBtn.textContent = "Pause";
 		mediaRecorder.resume();
-
 		stopBtn.disabled = false;
 	}
-
 	recBtn.disabled = true;
 	pauseResBtn.disabled = false;
-
 }
 
 
 function log(message){
-  dataElement.innerHTML = dataElement.innerHTML+'<br>'+message ;
+	dataElement.innerHTML = dataElement.innerHTML+'<br>'+message ;
 }
 
 
 
 //browser ID
 function getBrowser(){
-
 	var nVer = navigator.appVersion;
 	var nAgt = navigator.userAgent;
 	var browserName  = navigator.appName;
@@ -243,5 +224,4 @@ function getBrowser(){
 
 
 	return browserName;
-
 }
